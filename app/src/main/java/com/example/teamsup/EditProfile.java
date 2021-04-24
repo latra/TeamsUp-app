1d529a63f59d6040e37454a9eb9e94fb380453f8
mjunyent <marcjunyent1999@gmail.com>
2021-04-24 16:54:53 +0200

    edit profile

--- app/src/main/java/com/example/teamsup/EditProfile.java
+++ app/src/main/java/com/example/teamsup/EditProfile.java
@@ -2,26 +2,56 @@ package com.example.teamsup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class EditProfile extends AppCompatActivity {
 
+    public static final String MyPREFERENCES = "MyPrefs" ;
+
     EditText usuario;
     EditText direccion;
     EditText mail;
+
+    Button btnGuardar;
+    SharedPreferences sharedpreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
+        Bundle b = this.getIntent().getExtras();
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
         usuario = findViewById(R.id.usuario_edit);
         direccion = findViewById(R.id.edit_direction);
         mail = findViewById(R.id.edit_mail);
-        Bundle extras = getIntent().getExtras();
 
-        direccion.setText(extras.getString("direccion"));
-        usuario.setText(extras.getString("nombreUsuario"));
-        mail.setText(extras.getString("mail"));
+        btnGuardar = findViewById(R.id.btnGuardar);
+
+        usuario.setText(b.getString("usuario"));
+        direccion.setText(b.getString("direccion"));
+        mail.setText(b.getString("mail"));
+
+
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent();
+                i.putExtra("usuario", String.valueOf(usuario.getText()));
+                i.putExtra("direccion", String.valueOf(direccion.getText()));
+                i.putExtra("mail", String.valueOf(mail.getText()));
+                setResult(RESULT_OK, i);
+                finish();
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/teamsup/UserInfo.java
+++ app/src/main/java/com/example/teamsup/UserInfo.java
@@ -1,5 +1,6 @@
 package com.example.teamsup;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -30,18 +31,29 @@ public class UserInfo extends AppCompatActivity {
         editbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Bundle b = new Bundle();
                 Intent i = new Intent(UserInfo.this, EditProfile.class);
-                b.putString("direccion", (String) direccion.getText());
-                b.putString("nombreUsuario", (String) usuario.getText());
-                b.putString("mail", (String) mail.getText());
-                i.putExtras(b);
-                startActivity(i);
+                i.putExtra("usuario", (String) usuario.getText());
+                i.putExtra("direccion", (String) direccion.getText());
+                i.putExtra("mail", (String) mail.getText());
+                startActivityForResult(i, 2);
             }
         });
-
-
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==2 && resultCode==RESULT_OK && data!=null){
+            System.out.println("-----------------------------------------" + data.getStringExtra("usuario"));
+
+            usuario.setText(data.getStringExtra("usuario"));
+            direccion.setText(data.getStringExtra("direccion"));
+            mail.setText(data.getStringExtra("mail"));
+        }else{
+            direccion.setText("error");
+        }
+    }
+
     public void createEvent(View view) {
         Intent intent = new Intent(this, CreateEvent.class);
         startActivity(intent);
