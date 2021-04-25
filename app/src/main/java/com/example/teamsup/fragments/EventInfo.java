9821aad621b6ece1a55fcd5c808e94566d6846bf
mjunyent <marcjunyent1999@gmail.com>
2021-04-25 10:32:49 +0200

    merge

--- app/src/main/java/com/example/teamsup/fragments/EventInfo.java
+++ app/src/main/java/com/example/teamsup/fragments/EventInfo.java
@@ -1,16 +1,22 @@
 package com.example.teamsup.fragments;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.teamsup.R;
 
-public class EventInfo extends AppCompatActivity {
+public class EventInfo extends Fragment {
+
+    public EventInfo() {
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event_info);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
