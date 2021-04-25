becc20ba10641a149a3ab3a746902d8ddc3fb47e
Paula (Latra) Gallucci <personadelmonton@gmail.com>
2021-04-25 06:55:44 +0200

    Solicitud de acceso GPS + Correccion ejecucion al guardar preferencias

--- app/src/main/java/com/example/teamsup/activities/TemplateActivity.java
+++ app/src/main/java/com/example/teamsup/activities/TemplateActivity.java
@@ -1,18 +1,38 @@
 package com.example.teamsup.activities;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.example.teamsup.BuildConfig;
 import com.example.teamsup.fragments.Home;
 import com.example.teamsup.R;
 import com.example.teamsup.fragments.UserInfo;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class TemplateActivity extends AppCompatActivity {
+    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +44,56 @@ public class TemplateActivity extends AppCompatActivity {
         ImageView homeIcon = findViewById(R.id.HomeIco);
         userIcon.setOnClickListener((view) -> updateFragment(new UserInfo()));
         homeIcon.setOnClickListener((view) -> updateFragment(new Home()));
+
+        checkPermissions();
+
     }
 
-    public void updateFragment(Fragment fragment){
+    public void updateFragment(Fragment fragment) {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.fragment_place, fragment);
         ft.commit();
     }
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+        } else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ShowGrantPermissionsInfo();
+        } else {
+            // You can directly ask for the permission.
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
+    public void ShowGrantPermissionsInfo() {
+        showSnackbar(R.string.permission_denied_explanation,
+                R.string.settings, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Build intent that displays the App settings screen.
+                        Intent intent = new Intent();
+                        intent.setAction(
+                                Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package",
+                                BuildConfig.APPLICATION_ID, null);
+                        intent.setData(uri);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                });
+
+    }
+
+    private void showSnackbar(final int mainTextStringId, final int actionStringId,
+                              View.OnClickListener listener) {
+        Snackbar.make(
+                findViewById(android.R.id.content),
+                getString(mainTextStringId),
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction(getString(actionStringId), listener).show();
+    }
+
 }
