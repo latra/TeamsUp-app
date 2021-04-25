ad027077ddc634444bdccfc2e3bcaf861e40aff8
mjunyent <marcjunyent1999@gmail.com>
2021-04-25 10:22:22 +0200

    eventinfo

---
+++ app/src/main/java/com/example/teamsup/fragments/EventInfo.java
@@ -0,0 +1,16 @@
+package com.example.teamsup.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.teamsup.R;
+
+public class EventInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_info);
+    }
+}
