0a472ccbeeff87e793aaf34c1db58866b14d33c4
mjunyent <marcjunyent1999@gmail.com>
2021-05-31 23:04:40 +0200

    notifications from console ok

--- app/src/main/java/com/example/teamsup/Messaging/MessagingService.java
+++ app/src/main/java/com/example/teamsup/Messaging/MessagingService.java
@@ -30,6 +30,9 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class MessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MessagingService";
+
     FirebaseAuth mAuth;
     String uID;
     DatabaseReference myUser;
@@ -39,7 +42,18 @@ public class MessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
 
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+        sendNotification(remoteMessage.getNotification().getBody());
     }
 
     @Override
